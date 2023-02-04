10b097d365ed990aff1d093e9662c12f613bd58d
Jasmine-YongWoon <hanyw6401@gmail.com>
2023-02-04 16:01:03 +0900

    31-최초배포

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/heropy";
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'JASMINE / HAN YONGWOON',
+  email: 'hanyw6401@gmail.com',
+  blog: 'https://github.com/Jasmine-YongWoon',
+  github: 'https://github.com/Jasmine-YongWoon/movie-app',
+  repository: 'https://github.com/Jasmine-YongWoon/movie-app'
+})
